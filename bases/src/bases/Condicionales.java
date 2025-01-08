5dcec375069f5d8d2f6ba6815208491fa30f781f
javier <javier@ipartekaula.com>
2025-01-08 20:06:13 +0100

    Condicionales

---
+++ bases/src/bases/Condicionales.java
@@ -0,0 +1,32 @@
+package bases;
+
+import java.util.Scanner;
+
+public class Condicionales {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Dime un número");
+
+		int numeroElegido = sc.nextInt();
+
+		if (numeroElegido > 5) {
+			System.out.println("Es mayor que 5");
+		} else if (numeroElegido < 5) {
+			System.out.println("Es menor que 5");
+		} else {
+			System.out.println("Es igual a 5");
+		}
+
+		switch (numeroElegido) {
+		case 1:
+			System.out.println("Uno");
+			break;
+		case 2:
+			System.out.println("Dos");
+			break;
+		default:
+			System.out.println("Otro número");
+		}
+	}
+}
