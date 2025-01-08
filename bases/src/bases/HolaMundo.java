0858d19215fbba78e5940f435e8f3bddbb3f3572
javier <javier@ipartekaula.com>
2025-01-08 20:05:45 +0100

    Hola Mundo

---
+++ bases/src/bases/HolaMundo.java
@@ -0,0 +1,8 @@
+package bases;
+
+public class HolaMundo {
+	// main CTRL+ESPACIO genera la línea de abajo
+	public static void main(String[] args) {
+		System.out.println("Hola a todos"); // syso
+	}
+}
