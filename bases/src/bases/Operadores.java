4c17495fc74dcf0d83ca12f8fce801c4d267bb48
javier <javier@ipartekaula.com>
2025-01-09 17:38:31 +0100

    Operadores

---
+++ bases/src/bases/Operadores.java
@@ -0,0 +1,17 @@
+package bases;
+
+public class Operadores {
+	public static void main(String[] args) {
+		int x = 5;
+		
+		System.out.println(x);
+
+		System.out.println(x++);
+
+		System.out.println(x);
+
+		System.out.println(++x);
+
+		System.out.println(x);
+	}
+}
