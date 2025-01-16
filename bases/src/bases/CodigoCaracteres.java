8d9c659333ff3cb688c445ae91a2cf0a011e2b30
javier <javier@ipartekaula.com>
2025-01-16 16:22:04 +0100

    Tabla de código de caracteres

---
+++ bases/src/bases/CodigoCaracteres.java
@@ -0,0 +1,9 @@
+package bases;
+
+public class CodigoCaracteres {
+	public static void main(String[] args) {
+		for (char c = ' '; c <= 'z'; c++) {
+			System.out.printf("%d = %c\n", (int)c, c);
+		}
+	}
+}
