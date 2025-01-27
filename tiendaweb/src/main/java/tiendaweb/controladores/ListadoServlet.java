e64f17306f0717737eb7f48406b196c9ff3bae09
javier <javier@ipartekaula.com>
2025-01-27 16:25:33 +0100

    Modificamos el controlador para usar BDD

--- tiendaweb/src/main/java/tiendaweb/controladores/ListadoServlet.java
+++ tiendaweb/src/main/java/tiendaweb/controladores/ListadoServlet.java
@@ -1,6 +1,11 @@
 package tiendaweb.controladores;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import jakarta.servlet.ServletException;
@@ -13,18 +18,32 @@ import tiendaweb.modelos.Producto;
 @WebServlet("/listado")
 public class ListadoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String URL = "jdbc:sqlite:tiendaweb.sqlite";
+	private static final String SQL_SELECT = "SELECT * FROM productos";
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //		Recoger información de la petición
 //		Convertir la información recibida
 //		Crear objetos en base a la información recibida
 //		Ejecutar lógica de negocio
+		
 		ArrayList<Producto> productos = new ArrayList<Producto>();
 		
-		productos.add(new Producto(1L, "Producto 1", 12.12, 1.0));
-		productos.add(new Producto(2L, "Producto 2", 22.12, 2.0));
-		productos.add(new Producto(3L, "Producto 3", 32.12, 3.0));
-		productos.add(new Producto(4L, "Producto 4", 42.12, 4.0));
+		try {
+			Class.forName("org.sqlite.JDBC");
+			
+			try (Connection con = DriverManager.getConnection(URL);
+					Statement st = con.createStatement();
+					ResultSet rs = st.executeQuery(SQL_SELECT)) {
+				
+				while(rs.next()) {
+					productos.add(new Producto(rs.getLong("id"), rs.getString("nombre"), rs.getDouble("precio"), rs.getDouble("descuento")));
+				}
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 //		Preparar los objetos para la siguiente vista
 		request.setAttribute("productos", productos);
